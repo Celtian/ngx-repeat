66d416d6028ef36e3018ecce26e2b7accfa6a307
Dominik Hladík <dominik.hladik@seznam.cz>
2024-05-29 23:38:07 +0200

    chore(jest): jest optimized

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,4 @@
+import { swcAngularJestTransformer } from '@jscutlery/swc-angular-preset';
 import type { Config } from 'jest';
 
 const config: Config = {
@@ -8,7 +9,18 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/projects/ngx-repeat/**/*.spec.ts']
+  testMatch: ['<rootDir>/projects/ngx-repeat/**/*.spec.ts'],
+  transform: {
+    '^.+\\.(ts|mjs|js)$': swcAngularJestTransformer(),
+    '^.+\\.(html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)']
 };
 
 export default config;
